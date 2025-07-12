d0696b162d18b251f82b47d0522a8459188c5e1d
ILKHOM NAZHMUDINOV <rotor5335@gmail.com>
2025-07-12 08:53:28 +0500

    Update test_api_users.py

--- api-tests/test_api_users.py
+++ api-tests/test_api_users.py
@@ -1,1 +1,13 @@
+# test_api_users.py
+import requests
 
+def test_get_users_list():
+    url = "https://reqres.in/api/users?page=2"
+    response = requests.get(url)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "data" in data
+    assert len(data["data"]) > 0
+    assert data["data"][0]["email"].endswith("@reqres.in")
